c49e78658e1578460d21fe57c416f8a6207a8ca5
Hennan Gadelha <hennanhennan@gmail.com>
2021-03-28 13:10:22 -0300

    listando usuarios na datatables

--- src/main/java/com/mballem/curso/security/repository/UsuarioRepository.java
+++ src/main/java/com/mballem/curso/security/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.mballem.curso.security.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,5 +13,10 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	@Query("SELECT u from Usuario u WHERE u.email like :email")
 	public  Usuario findByEmail(@Param("email") String email);
+
+	@Query("SELECT u from Usuario u " 
+				+ "join u.perfis p " 
+					+ "WHERE u.email like :search% OR p.desc like :search%")
+	public Page<Usuario> findByEmailOrPerfil(String search, Pageable pageable);
 
 }
--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -1,14 +1,21 @@
 package com.mballem.curso.security.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mballem.curso.security.domain.Usuario;
+import com.mballem.curso.security.service.UsuarioService;
 
 @Controller @RequestMapping("u")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioService service;
 	
 	@GetMapping("/novo/cadastro/usuario")
 	public String cadastroPorAdminParaAdminMedicoPaciente(Usuario usuario) {
@@ -17,7 +24,17 @@ public class UsuarioController {
 	}
 	
 	
+	//listar usuarios
+	@GetMapping("/lista")
+	public String listarUsuarios() {
+		return "usuario/lista";
+	}
 
+	@GetMapping("/datatables/server/usuarios")
+	public ResponseEntity<?> listarUsuariosDatatables(HttpServletRequest request) {
+		return ResponseEntity.ok(service.buscarTodos(request));
+		
+	}
 	
 	
 }
