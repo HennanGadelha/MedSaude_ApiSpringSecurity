5cfd466ea929bd35165cc9a5ac4549f598db0f15
Hennan Gadelha <hennanhennan@gmail.com>
2021-04-03 22:31:45 -0300

    corrigindo regra de autenticaçoes

--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,20 +44,26 @@ public class UsuarioController {
 	}
 	
 	//salvar cadastro de usuarios por adm
-	@SuppressWarnings("unlikely-arg-type")
+
 	@PostMapping("/cadastro/salvar")
 	public String salvarUsuarios(Usuario usuario, RedirectAttributes attr) {
 		
 		List<Perfil> perfis = usuario.getPerfis();
 		if(perfis.size() > 2 || 
-				perfis.contains(Arrays.asList(new Perfil(1L), new Perfil(3L))) ||
-					perfis.contains(Arrays.asList(new Perfil(2L), new Perfil(3L)))) {
+				perfis.containsAll(Arrays.asList(new Perfil(1L), new Perfil(3L))) ||
+					perfis.containsAll(Arrays.asList(new Perfil(2L), new Perfil(3L)))) {
 			
 			attr.addFlashAttribute("falha", "Paciente não pode ser admin e/ou medico");
 			attr.addFlashAttribute("usuario", usuario);
 		}else {
-			service.salvarUsuario(usuario);
-			attr.addFlashAttribute("suceso", "operaçao realizada com sucesso");
+			try {
+				service.salvarUsuario(usuario);
+				attr.addFlashAttribute("sucesso", "operaçao realizada com sucesso");
+			} catch (DataIntegrityViolationException e) {
+				
+				attr.addFlashAttribute("falha", "cadastro já realiazado");
+				e.printStackTrace();
+			}
 		}
 		
 		return "redirect:/u/novo/cadastro/usuario";
